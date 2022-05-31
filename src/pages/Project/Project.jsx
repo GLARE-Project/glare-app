259532b08098f763446655c3aac92e67740ee96a
Ethan <ehancoc4@kent.edu>
2022-05-30 22:29:24 -0400

    🐛 New project fix

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -28,8 +28,28 @@ const Project = ({ user, dispatch, project, setProject }) => {
     );
 
     let imageURL = await api.getMedia(Server.imageBucketID, response.$id);
+    
+    const hotspot = JSON.stringify({
+      name: "",
+      hotspot_id: "",
+      position: null,
+      latitude: 0,
+      longitude: 0,
+      overlay: "",
+      virtual_object: "",
+      isSubHotspot: false,
+      panorama_image: "",
+      overlay_size: 10,
+      overlay_offset_x: 0,
+      overlay_offset_y: 0,
+      start_audio: "",
+      main_pages: [],
+      media_pages: [],
+    });
 
-    const imageP = { ...currentProject, homepage_image: imageURL.href };
+    const imageP = { ...currentProject, homepage_image: imageURL.href, hotspots: [hotspot] };
+
+    console.log(imageP);
 
     let response2 = await api.createMedia(
       Server.audioBucketID,
@@ -93,27 +113,27 @@ const Project = ({ user, dispatch, project, setProject }) => {
     <>
       <section className="container h-[80vh] max-h-screen px-3 max-w-2xl mx-auto flex flex-col">
         {isError && <Alert color="red" message="Something went wrong..." />}
-        <div className="my-auto px-16 rounded-lg text-center">
-          <div className="font-bold text-3xl md:text-5xl lg:text-6xl">
+        <div className="px-16 my-auto text-center rounded-lg">
+          <div className="text-3xl font-bold md:text-5xl lg:text-6xl">
             📝 <br /> &nbsp; Create a New Project
           </div>
 
           <form onSubmit={handleAddProject}>
             <input
               type="text"
-              className="w-full my-8 px-6 py-4 text-xl rounded-lg border-0 focus:ring-2 focus:ring-gray-800 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl shadow-md"
+              className="w-full px-6 py-4 my-8 text-xl transition duration-200 ease-in-out transform border-0 rounded-lg shadow-md focus:ring-2 focus:ring-gray-800 hover:-translate-y-1 hover:scale-110 hover:shadow-xl"
               placeholder="Project Name"
               value={currentProject.project_name}
               onChange={(e) => createProject(e.target.value)}
             ></input>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-5">
+            <div className="grid grid-cols-1 gap-4 mb-5 md:grid-cols-2">
               <div>
-                <p className="text-sm text-left pb-2 text-gray-500">
+                <p className="pb-2 text-sm text-left text-gray-500">
                   Home Page Image
                 </p>
                 {image ? (
                   <img
-                    className="rounded-lg shadow-lg border my-6"
+                    className="my-6 border rounded-lg shadow-lg"
                     src={URL.createObjectURL(image)}
                   />
                 ) : (
@@ -125,22 +145,17 @@ const Project = ({ user, dispatch, project, setProject }) => {
                   accept="image/*"
                   name="homepage_image"
                   onChange={(e) => setImage(e.target.files[0])}
-                  className="block w-full text-sm text-slate-500
-      file:mr-4 file:py-2 file:px-4
-      file:rounded-full file:border-0
-      file:text-sm file:font-semibold
-      file:bg-sky-50 file:text-sky-700
-      hover:file:bg-sky-100"
+                  className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-sky-50 file:text-sky-700 hover:file:bg-sky-100"
                 />
               </div>
 
               <div>
-                <p className="text-sm text-left pb-2 text-gray-500">
+                <p className="pb-2 text-sm text-left text-gray-500">
                   Intro Audio
                 </p>
                 {audio ? (
                   <audio
-                    className="rounded-full shadow-lg border my-6"
+                    className="my-6 border rounded-full shadow-lg"
                     controls
                   >
                     <source src={URL.createObjectURL(audio)} />
@@ -154,19 +169,14 @@ const Project = ({ user, dispatch, project, setProject }) => {
                   accept="audio/*"
                   name="intro_audio"
                   onChange={(e) => setAudio(e.target.files[0])}
-                  className="block w-full text-sm text-slate-500
-      file:mr-4 file:py-2 file:px-4
-      file:rounded-full file:border-0
-      file:text-sm file:font-semibold
-      file:bg-sky-50 file:text-sky-700
-      hover:file:bg-sky-100"
+                  className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-sky-50 file:text-sky-700 hover:file:bg-sky-100"
                 />
               </div>
             </div>
 
             <input
               type="submit"
-              className="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
+              className="px-12 py-3 mx-auto mt-4 font-semibold text-gray-900 bg-white border border-gray-900 rounded-lg shadow-md text-md hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
             />
           </form>
 
@@ -174,10 +184,10 @@ const Project = ({ user, dispatch, project, setProject }) => {
         </div>
       </section>
 
-      <section className="absolute bottom-0 right-0 py-3 px-6 mr-8 mb-8">
+      <section className="absolute bottom-0 right-0 px-6 py-3 mb-8 mr-8">
         <button
           onClick={handleLogout}
-          className="mx-auto mt-4 py-3 px-12 font-semibold text-md rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
+          className="px-12 py-3 mx-auto mt-4 font-semibold text-gray-900 bg-white border border-gray-900 rounded-lg shadow-md text-md hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
         >
           Logout 👋
         </button>
