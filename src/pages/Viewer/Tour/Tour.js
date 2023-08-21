13ad630d89cf7fe725471889f3196e9c57a898a5
Chris L <clenart4@kent.edu>
2023-08-21 00:28:48 -0400

    Add min styling back to tour page

--- src/pages/Viewer/Tour/Tour.js
+++ src/pages/Viewer/Tour/Tour.js
@@ -14,6 +14,7 @@ import {
   MenuAlt1Icon,
 } from "@heroicons/react/solid";
 import MenuOverlay from "./MenuItem.jsx";
+import './Tour.css';
 
 const Loader = () => {
   const { active } = useProgress();
